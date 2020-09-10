d29aa4482f9bc93c2d3834d86bb3a158981e6aef
BDooley11 <barrydooley11@gmail.com>
2020-09-10 11:31:02 +0100

    update images and readme

--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
 export default [
   {
     pics: [
-      "./Data Images/1_1.jpeg",
-      "./Data Images/1_2.jpeg",
-      "./Data Images/1_3.jpeg"
+      "./Data Images/1_1.jpg",
+      "./Data Images/1_2.jpg",
+      "./Data Images/1_3.jpg"
     ],
-    dealer: "Boland Wexford",
+    dealer: "BarryD Car Sales",
     model: "Ford Focus Titanium 1.5td 95ps 6spd",
     year: 2018,
     brief: "1.5 Diesel",
@@ -16,11 +16,11 @@ export default [
   },
   {
       pics: [
-        "./Data Images/2_1.jpeg",
-        "./Data Images/2_2.jpeg",
-        "./Data Images/2_3.jpeg"
+        "./Data Images/2_1.jpg",
+        "./Data Images/2_2.jpg",
+        "./Data Images/2_3.jpg"
       ],
-      dealer: "Boland Wexford",
+      dealer: "BarryD Car Sales",
       model: "Ford Focus Style 1.5 TD 95ps 6spd 4",
       year: 2017,
       brief: "1.5 Diesel",
@@ -31,11 +31,11 @@ export default [
     },
     {
       pics: [
-        "./Data Images/3_1.jpeg",
-        "./Data Images/3_2.jpeg",
-        "./Data Images/3_3.jpeg"
+        "./Data Images/3_1.jpg",
+        "./Data Images/3_2.jpg",
+        "./Data Images/3_3.jpg"
       ],
-      dealer: "Boland Wexford",
+      dealer: "BarryD Car Sales",
       model: "Kia Sorento Platinum 4X2 5DR",
       year: 2017,
       brief: "2.2 Diesel",
